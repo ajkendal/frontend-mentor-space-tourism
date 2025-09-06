eaf3053b8f5ace4db0d1da7f36c1b3c17b0a23fc
Amanda J Kendal-Brown <15990675+ajkendal@users.noreply.github.com>
2025-09-06 15:17:40 -0700

    Revert "Restructure app and add new pages"
    This reverts commit 427416fcf1d8fa7fae134e8388969f01d7df7992.

--- app/destination/page.tsx
+++
@@ -1,5 +0,0 @@
-const Destination = () => {
-  return <div>Destination</div>;
-};
-
-export default Destination;
--- app/technology/page.tsx
+++
@@ -1,5 +0,0 @@
-const Technology = () => {
-  return <div>Technology</div>;
-};
-
-export default Technology;
